4814f2dd1eaf5d0d6d28e5b8b40ade3af21ac8a8
shivamach <shivamach13@gmail.com>
2020-05-18 00:38:38 +0530

    now the dictionary works well

--- Dictionary/v0.1/dictionary.py
+++ Dictionary/v0.1/dictionary.py
@@ -12,12 +12,35 @@ while(True):
         print("closing....")
         break
     close_list = get_close_matches(word, keys)
-    word = close_list[0]
-    if len(close_list)==0:
-        print("No such word found")
-    elif word in data:
-        for (i,item) in enumerate(data[word]):
-            print(f"{i} {item}")
+    if word in data:
+        result = data[word]
+    elif word.upper() in data:
+        result = data[word.upper()]
+    elif word.title() in data:
+        result = data[word.title()]
+    elif len(close_list)>0:
+        print("Did you mean any of these words- ")
+        for (i,item) in enumerate(close_list):
+            print(f"{i} - {item}")
+        print("Enter n if none of the words matched, or their index if they do")
+        inp = input()
+        if inp == 'n':
+            print("re-enter the word")
+            continue
+        else:
+            inp = int(inp)
+            result = data[close_list[inp]]
+    else:
+        result = 000
+
+    if result == 000:
+        print("no such word found")
+    else:
+        for (i,item) in enumerate(result):
+            print(f"{i}- {item}")
+
+
+
 
 
 
