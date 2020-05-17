cbc09ca695e964171d46716dbb52f87bd8043b2f
shivamach <shivamach13@gmail.com>
2020-05-18 00:05:04 +0530

    i wanna break my head over this stuff

--- Dictionary/v0.1/dictionary.py
+++ Dictionary/v0.1/dictionary.py
@@ -1,10 +1,26 @@
-import main
-# imports
+import json
+from difflib import get_close_matches
 
-main.setup("original.json")
-search = input(":")
-output = main.translate(search)
-if output != 0:
-    main.printdef(output)
+data = json.load(open("original.json"))
+keys = data.keys()
+
+print("Press q to quit")
+while(True):
+    print("Enter the word: ")
+    word = input()
+    if word == 'q':
+        print("closing....")
+        break
+    close_list = get_close_matches(word, keys)
+    word = close_list[0]
+    if len(close_list)==0:
+        print("No such word found")
+    elif word in data:
+        for (i,item) in enumerate(data[word]):
+            print(f"{i} {item}")
+
+
+
+
 
 
--- Dictionary/v0.1/main.py
+++ Dictionary/v0.1/main.py
@@ -1,20 +1,30 @@
 import json
-#imports
+
+
+# imports
 
 
 def setup(filename):
     global data
     data = json.load(open(filename))
+    global keys
+    keys = data.key()
+
 
 def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     else:
         print("word does not exist in database")
         return 0
 
+
 def printdef(output):
     '''this function sees the no of items in the list and prints them as numerical bulleted points'''
-    for (i,item) in enumerate(output):
+    for (i, item) in enumerate(output):
         print(f"{i} {item}")
